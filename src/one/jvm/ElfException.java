8219945530da395a42456ef233ab9a35ff68aa7b
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-26 13:40:30 +0300

    Построение логики для действия с флагами JVM

---
+++ src/one/jvm/ElfException.java
@@ -0,0 +1,23 @@
+package one.jvm;
+
+import java.io.IOException;
+
+public class ElfException extends IOException {
+
+    public ElfException() {
+    }
+
+    public ElfException(String message) {
+        super(message);
+    }
+
+    public ElfException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ElfException(Throwable cause) {
+        super(cause);
+
+    }
+
+}
